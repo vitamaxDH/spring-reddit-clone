b9ac08d32c9e08fbd68936af3dcb61f4cee7949f
daehan.choi <daehan.choi@epapyrus.com>
2020-10-05 23:54:18 +0900

    Add some class files

---
+++ src/main/java/com/example/springredditclone/service/AuthService.java
@@ -0,0 +1,47 @@
+package com.example.springredditclone.service;
+
+import com.example.springredditclone.dto.RegisterRequest;
+import com.example.springredditclone.model.User;
+import com.example.springredditclone.model.VerificationToken;
+import com.example.springredditclone.repository.UserRepository;
+import com.example.springredditclone.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}
