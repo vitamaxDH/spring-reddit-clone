c7a18e79013e7778a331081a1871a4423710552e
vitamaxDH <hellonewtry@gmail.com>
2020-10-14 00:36:05 +0900

    feat:

--- src/main/java/com/example/springredditclone/mapper/Subredditmapper.java
+++ src/main/java/com/example/springredditclone/mapper/Subredditmapper.java
@@ -1,5 +1,18 @@
 package com.example.springredditclone.mapper;
 
+import com.example.springredditclone.dto.SubredditDto;
+import com.example.springredditclone.model.Post;
+import com.example.springredditclone.model.Subreddit;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface Subredditmapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post>numberOfPosts) { return numberOfPosts.size(); };
 }
